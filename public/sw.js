06df08c25f21ec80a7dcea503adae24bd8a4900b
pipopotamasu <ec0156hx@gmail.com>
2018-08-21 20:24:58 +0900

    steal request

--- public/sw.js
+++ public/sw.js
@@ -2,3 +2,11 @@ self.addEventListener("install", function(event) {
   console.log("service worker installed!");
   console.log(event);
 });
+
+self.addEventListener("fetch", function(e) {
+  console.info("fetch", e);
+
+  if (e.request.url.indexOf("test") != -1) {
+    e.respondWith(new Response("Hello world"));
+  }
+});
